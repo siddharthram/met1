2d8fd7f85a7f619a15ea8e8b00e7ed0c3b5c63d8
Siddharth Ram <siddharthram@gmail.com>
2013-09-29 09:28:21 -0700

    ch3

--- lendlib.js
+++ lendlib.js
@@ -17,7 +17,33 @@ if (Meteor.isClient) {
 
   Template.categories.lists = function() {
     return lists.find({}, {sort: {Category:1}});
+  };
+  Session.set('adding_category',false);
+  Template.categories.new_cat = function() {
+    return Session.equals('adding_category', true);
   }
+  Template.categories.events({
+    'click #btnNewCat': function(e,t)
+    {Session.set('adding_category', true); Meteor.flush();
+    focusText(t.find("#add-category"));
+  },
+  'keyup #add-category': function(e,t){
+    if (e.which === 13){
+      var catVal = String(e.target.value || "");
+      if (catVal){
+        lists.insert({Category:catVal});
+        Session.set('adding_category', false);
+      }
+    }
+  }
+  });
+  /////Generic Helper Functions/////
+//this function puts our cursor where it needs to be.
+function focusText(i) {
+  i.focus();
+  i.select();
+};
+
 }
 
 if (Meteor.isServer) {
